f99538720561ceefc222eb6d1ae3ef80993756f0
Philipp Stephani <st_philipp@yahoo.de>
2011-04-05 01:27:38 +0200

    more correct location for fontconfig.h

--- fonts_fontconfig.c
+++ fonts_fontconfig.c
@@ -24,7 +24,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdlib.h>
 #include <string.h>
 
-#include <fontconfig.h>
+#include <fontconfig/fontconfig.h>
 
 #include "fonts_impl.h"
 
