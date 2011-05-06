9f7c581ff6c8bef909e787de411890fd009a5f36
Philipp Stephani <st_philipp@yahoo.de>
2011-05-06 14:00:45 +0200

    shorter name for CTAN archive

--- ctan-archive.sh
+++ ctan-archive.sh
@@ -6,4 +6,4 @@ shopt -s failglob
 
 make
 
-zip -v lualatex-platform-ctan.zip README *.m4 *.ac *.am *.in src/*.am src/*.in src/*.c src/*.h lualatex-platform.{lua,sty,html}
+zip -v ctan.zip README *.m4 *.ac *.am *.in src/*.am src/*.in src/*.c src/*.h lualatex-platform.{lua,sty,html}
