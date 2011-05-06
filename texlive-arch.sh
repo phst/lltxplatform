79959e20befbcf03ea3a407ce8f10b880a0dd0dd
Philipp Stephani <st_philipp@yahoo.de>
2011-05-06 13:59:10 +0200

    a small script to find out the current TeX Live architecture
    recognizes only three architectures at the moment

---
+++ texlive-arch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+system="$(uname -s)"
+machine="$(uname -m)"
+
+case "$system" in
+    MINGW*)
+        arch=win32 ;;
+    Darwin)
+        [[ "$(sysctl -n hw.optional.x86_64)" == 1 ]] && arch=x86_64-darwin ;;
+    Linux)
+        case "$machine" in
+            x86_64|i386) arch="$machine-linux" ;;
+        esac
+        ;;
+esac
+
+if [[ -n "$arch" ]]
+then
+    echo "$arch"
+else
+    exit 1
+fi
