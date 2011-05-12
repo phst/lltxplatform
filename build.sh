33f714e2e7e871fa29621c34f15ce2ac87b2fa76
Philipp Stephani <st_philipp@yahoo.de>
2011-05-13 00:23:53 +0200

    allow cross-compile unter Linux i386

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+rm -f built-archs.lst
+
 if [[ "$1" == -c ]]
 then
     configure=1
@@ -29,13 +31,18 @@ build() {
         cd "$build_dir"
         make install
     )
+    echo "$arch" >> built-archs.lst
 }
 
 native_arch="$(bash texlive-arch.sh)"
 
 build "$native_arch" "$@"
 
-if [[ "$native_arch" == x86_64-darwin ]]
-then
-    build universal-darwin --disable-dependency-tracking --build=i386-apple-darwin9.0 "$@"
-fi
+case "$native_arch" in
+    x86_64-darwin)
+        build universal-darwin --disable-dependency-tracking --host=i386-apple-darwin9.0 "$@" ;;
+    i386-linux)
+        prefix=i586-mingw32msvc
+        which "$prefix-gcc" > /dev/null && build win32 "--host=$prefix" "$@"
+        build x86_64-linux --host=x86_64-linux CFLAGS=-m64 "$@" ;;
+esac
--- tlcontrib-package.sh
+++ tlcontrib-package.sh
@@ -13,15 +13,6 @@ srcdir="$root/source/lualatex/$package"
 arch="$(bash texlive-arch.sh)"
 stage="stage/$arch"
 
-if [[ "$arch" == win32 ]]
-then
-    libext=dll
-else
-    libext=so
-fi
-
-libfile="$library.$libext"
-
 bash build.sh
 
 base_pkg() {
@@ -39,11 +30,16 @@ base_pkg() {
 binary_pkg() {
     local arch="$1"
     local libdir="bin/$arch/lib/lualatex/lua/$package"
+    [[ "$arch" == win32 ]] && local libext=dll || local libext=so
+    local libfile="$library.$libext"
     install -v -d "$libdir"
     install -v -c -m 755 "stage/$arch/lib/$libfile" "$libdir"
     zip -v -r "tlcontrib-$arch.zip" "$libdir/$libfile"
 }
 
 base_pkg
-binary_pkg "$arch"
-[[ "$arch" == x86_64-darwin ]] && binary_pkg universal-darwin
+
+while read -r arch
+do
+    binary_pkg "$arch"
+done < built-archs.lst
