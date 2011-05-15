ecb263af30d57fb938d3956ad1699eabaece9799
Philipp Stephani <st_philipp@yahoo.de>
2011-05-15 19:27:30 +0200

    use kCFNull constant instead of empty string

--- src/fonts_ats.c
+++ src/fonts_ats.c
@@ -42,77 +42,75 @@ int lltxplatform_get_installed_fonts_impl(struct lltxplatform_fontinfo **const f
     if (names != NULL) {
       const CFMutableArrayRef paths = CFArrayCreateMutable(kCFAllocatorDefault, 0l, &kCFTypeArrayCallBacks);
       if (paths != NULL) {
-        CFStringRef empty = CFSTR("");
-        if (empty != NULL) {
-          ATSFontRef font;
-          OSStatus result;
-          while ((result = ATSFontIteratorNext(iter, &font)) == noErr) {
-            FSRef file;
-            CFStringRef name = NULL;
-            CFStringRef path = NULL;
-            if (ATSFontGetName(font, kATSOptionFlagsDefault, &name) == noErr) {
-              CFArrayAppendValue(names, name);
-              CFRelease(name);
-            } else {
-              CFArrayAppendValue(names, empty);
-            }
-            if (ATSFontGetFileReference(font, &file) == noErr) {
-              CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, &file);
-              if (url != NULL) {
-                path = CFURLCopyFileSystemPath(url, (CFURLPathStyle) kCFURLPOSIXPathStyle);
-                CFRelease(url);
-              }
-            }
-            if (path != NULL) {
-              CFArrayAppendValue(paths, path);
-              CFRelease(path);
-            } else {
-              CFArrayAppendValue(paths, empty);
+        ATSFontRef font;
+        OSStatus result;
+        while ((result = ATSFontIteratorNext(iter, &font)) == noErr) {
+          CFStringRef name;
+          FSRef file;
+          CFStringRef path = NULL;
+          if (ATSFontGetName(font, kATSOptionFlagsDefault, &name) == noErr) {
+            CFArrayAppendValue(names, name);
+            CFRelease(name);
+          } else {
+            CFArrayAppendValue(names, kCFNull);
+          }
+          if (ATSFontGetFileReference(font, &file) == noErr) {
+            const CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, &file);
+            if (url != NULL) {
+              path = CFURLCopyFileSystemPath(url, (CFURLPathStyle) kCFURLPOSIXPathStyle);
+              CFRelease(url);
             }
           }
-          if (result == kATSIterationCompleted) {
-            CFIndex cnt = CFArrayGetCount(names);
-            if (cnt > 0 && cnt < INT_MAX && cnt == CFArrayGetCount(paths)) {
-              struct lltxplatform_fontinfo *array = (struct lltxplatform_fontinfo *) calloc((size_t) cnt, sizeof(struct lltxplatform_fontinfo));
-              if (array != NULL) {
-                CFIndex i;
-                status = 0;
-                for (i = 0; i < cnt; ++i) {
-                  struct lltxplatform_fontinfo *info = &array[i];
-                  CFStringRef name = (CFStringRef) CFArrayGetValueAtIndex(names, i);
-                  CFStringRef path = (CFStringRef) CFArrayGetValueAtIndex(paths, i);
-                  info->name = NULL;
-                  info->path = NULL;
-                  if (name != NULL) {
-                    CFIndex length = CFStringGetLength(name);
-                    CFIndex size = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
-                    if (size > 1) {
-                      char *buffer = (char *) malloc((size_t) size);
-                      if (buffer != NULL) {
-                        if (CFStringGetCString(name, buffer, size, kCFStringEncodingUTF8)) {
-                          info->name = buffer;
-                        }
-                      }
-                    }
-                  }
-                  if (path != NULL) {
-                    CFIndex size = CFStringGetMaximumSizeOfFileSystemRepresentation(path);
-                    if (size > 0) {
-                      char *buffer = (char *) malloc((size_t) size);
-                      if (buffer != NULL) {
-                        if (CFStringGetFileSystemRepresentation(path, buffer, size)) {
-                          info->path = buffer;
-                        }
+          if (path != NULL) {
+            CFArrayAppendValue(paths, path);
+            CFRelease(path);
+          } else {
+            CFArrayAppendValue(paths, kCFNull);
+          }
+        }
+        if (result == kATSIterationCompleted) {
+          const CFIndex cnt = CFArrayGetCount(names);
+          if (cnt > 0 && cnt < INT_MAX && cnt == CFArrayGetCount(paths)) {
+            struct lltxplatform_fontinfo *const array = (struct lltxplatform_fontinfo *) calloc((size_t) cnt, sizeof(struct lltxplatform_fontinfo));
+            if (array != NULL) {
+              CFIndex i;
+              status = 0;
+              for (i = 0; i < cnt; ++i) {
+                struct lltxplatform_fontinfo *const info = &array[i];
+                const void *const raw_name = CFArrayGetValueAtIndex(names, i);
+                const void *const raw_path = CFArrayGetValueAtIndex(paths, i);
+                const CFStringRef name = raw_name != kCFNull ? (CFStringRef) raw_name : NULL;
+                const CFStringRef path = raw_path != kCFNull ? (CFStringRef) raw_path : NULL;
+                info->name = NULL;
+                info->path = NULL;
+                if (name != NULL) {
+                  const CFIndex length = CFStringGetLength(name);
+                  const CFIndex size = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
+                  if (size > 1) {
+                    char *const buffer = (char *) malloc((size_t) size);
+                    if (buffer != NULL) {
+                      if (CFStringGetCString(name, buffer, size, kCFStringEncodingUTF8)) {
+                        info->name = buffer;
                       }
                     }
                   }
                 }
-                *fonts = array;
-                *count = (unsigned int) cnt;
+                if (path != NULL) {
+                  const CFIndex size = CFStringGetMaximumSizeOfFileSystemRepresentation(path);
+                  if (size > 0) {
+                    char *const buffer = (char *) malloc((size_t) size);
+                    if (buffer != NULL) {
+                      if (CFStringGetFileSystemRepresentation(path, buffer, size)) {
+                        info->path = buffer;
+                      }
+                    }
+                  }
+                }
               }
+              *fonts = array;
+              *count = (unsigned int) cnt;
             }
           }
-          CFRelease(empty);
         }
         CFRelease(paths);
       }
